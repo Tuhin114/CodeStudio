12c6a0a91aba413ae62ce7b1170cfcdb1bedd9b0
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-26 10:26:37 +0530

    Find Duplicate in Array

---
+++ Find Duplicate in Array.cpp
@@ -0,0 +1,26 @@
+// CodeStudio Find Duplicate in Array
+// https://www.codingninjas.com/studio/problems/find-duplicate-in-array_1112602?leftPanelTabValue=PROBLEM
+
+// #include <bits/stdc++.h>
+
+// int findDuplicate(vector<int> &arr, int n){
+
+//     sort(arr.begin(),arr.end());
+
+//     for(int i = 0 ; i < n ; i++)
+
+//     {
+
+//         if(arr.at(i) == arr.at(i+1))
+
+//         {
+
+//             return arr.at(i);
+
+//             break;
+
+//         }
+
+//     }
+
+// }
