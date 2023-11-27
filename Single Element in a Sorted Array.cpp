77c6cb434bf25541103e475b49cc943ffc0d81c1
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-27 22:10:14 +0530

    Single Element in a Sorted Array

---
+++ Single Element in a Sorted Array.cpp
@@ -0,0 +1,16 @@
+// CodeStudio - Single Element in a Sorted Array
+// https://www.codingninjas.com/studio/problems/unique-element-in-sorted-array_1112654?interviewProblemRedirection=true&leftPanelTabValue=PROBLEM
+
+// int singleNonDuplicate(vector<int>& arr)
+// {
+// 	int s = 0;
+// 	int e = arr.size()-1;
+// 	while(s<=e){
+// 		if(arr[s]==arr[s+1]){
+// 			s=s+2;
+// 		}
+// 		else{
+// 			return arr[s];
+// 		}
+// 	}
+// }
