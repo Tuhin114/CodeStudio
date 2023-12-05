7298f4825d30573ac140227c914dc5f34d55037e
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-05 20:09:57 +0530

    Minimum Sum Subarray Of Given Size

---
+++ Minimum Sum Subarray.cpp
@@ -0,0 +1,21 @@
+// CodeStudio  Minimum Sum Subarray Of Given Size
+//  https://www.codingninjas.com/studio/problems/find-minimum-sum-subarray-of-given-size_873368?interviewProblemRedirection=true&leftPanelTabValue=PROBLEM
+
+// #include <bits/stdc++.h>
+// int minSubarraySum(int arr[], int n, int k)
+// {
+//     int sum =0;
+//     int mini = INT_MAX;
+//     for(int i  =0 ;i<k;i++)
+//     {
+//         sum += arr[i];
+//     }
+//     mini = min(mini,sum);
+//     for(int i =k;i<n;i++)
+//     {
+//         sum = sum+arr[i];
+//         sum = sum - arr[i-k];
+//         mini = min(mini,sum);
+//     }
+//     return mini;
+// }
