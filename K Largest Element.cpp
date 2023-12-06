4ab3410f7e6152aabac3f6313c0d2b930d6b400b
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-06 17:23:56 +0530

    CodeStudio K Largest Element

---
+++ K Largest Element.cpp
@@ -0,0 +1,12 @@
+// CodeStudio K Largest Element
+// https://www.codingninjas.com/studio/problems/k-largest-element_1062624?interviewProblemRedirection=true&leftPanelTabValue=PROBLEM
+
+// #include <bits/stdc++.h>
+// vector<int> Klargest(vector<int> &a, int k, int n) {
+//     sort(a.begin(),a.end());
+//     vector<int> ans;
+//     for(int i = n-k; i<=n; i++){
+//         ans.push_back(a[i]);
+//     }
+//     return ans;
+// }
