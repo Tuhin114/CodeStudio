c04c087c9e7e167cdb15464b81c92adb21fbe42e
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-10 18:31:35 +0530

    Codestudio- Magic Index

---
+++ Magical Index.cpp
@@ -0,0 +1,14 @@
+// Codestudio- Magic Index
+// https://www.codingninjas.com/studio/problems/magic-index_1199229?interviewProblemRedirection=true&leftPanelTabValue=PROBLEM
+
+// #include <bits/stdc++.h>
+// int magicIndex(vector<int> a, int n) {
+// 	int i=0;
+// 	while(i<=n-1){
+// 		if(a[i]==i){
+// 			return i;
+// 		}
+// 		i++;
+// 	}
+// 	return -1;
+// }
