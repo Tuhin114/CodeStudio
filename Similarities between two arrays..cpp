991ed51c0add7783ea3f4cb89a839bffae967b4b
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-13 18:06:25 +0530

    CodeStudio- Find similarities between two arrays.

---
+++ Similarities between two arrays..cpp
@@ -0,0 +1,20 @@
+// CodeStudio- Find similarities between two arrays.
+// https://www.codingninjas.com/studio/problems/find-similarities-between-two-arrays_1229070?interviewProblemRedirection=true&leftPanelTabValue=PROBLEM
+
+// #include <bits/stdc++.h>
+// pair < int , int > findSimilarity(vector < int > arr1, vector < int > arr2, int n, int m)
+// {
+// 	int count=0;
+// 	for(int i=0;i<n;i++){
+// 		for(int j=0;j<m;j++){
+// 			if(arr1[i]==arr2[j]){
+// 				count++;
+// 			}
+
+// 		}
+
+// 	}
+// 	pair<int,int>ans={count,n+m-count};
+// 	return ans;
+
+// }
