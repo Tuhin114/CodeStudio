c92f9f3608fce47d8c6966a779fa8cbfa28ba2fb
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-28 16:06:54 +0530

    CodeStudio - Linear Search

---
+++ Linear Search.cpp
@@ -0,0 +1,14 @@
+// CodeStudio - Linear Search
+// https://www.codingninjas.com/studio/problems/linear-search_6922070?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+// int linearSearch(int n, int num, vector<int> &arr)
+// {
+//     for(int i = 0; i < (n - 1); i++){
+//         if(arr[i] == num){
+//             return i;
+//             break;
+
+//         }
+//     }
+//     return -1;
+// }
