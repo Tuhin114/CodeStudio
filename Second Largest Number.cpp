ccf5a22860d3956f96438050944514fcd0d9d74d
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-29 22:17:21 +0530

    CodeStudio -  Second Largest Number

---
+++ Second Largest Number.cpp
@@ -0,0 +1,14 @@
+// CodeStudio -  Second Largest Number
+// https://www.codingninjas.com/studio/problems/ninja-and-the-second-order-elements_6581960?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+// vector<int> getSecondOrderElements(int n, vector<int> a) {
+
+//     vector<int> newVector = a;
+//     sort(newVector.begin(), newVector.end());
+//     vector<int> result;
+
+//     result.push_back(newVector[newVector.size() - 2]);
+//     result.push_back(newVector[1]);
+
+//    return result;
+// }
