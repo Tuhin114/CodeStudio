59e571c914378cb0703c3f61b225600ba3c8d265
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-29 22:02:27 +0530

    CodeStudio - Remove Duplicates from Sorted Array

---
+++ Remove Duplicates in Array.cpp
@@ -0,0 +1,18 @@
+// CodeStudio - Remove Duplicates from Sorted Array
+//  https://www.codingninjas.com/studio/problems/remove-duplicates-from-sorted-array_1102307?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+// int removeDuplicates(vector<int> &arr, int n) {
+// 	vector<int> ans;
+// 	int i = 0;
+
+// while (i < n - 1) {
+//     if (arr[i] != arr[i + 1]) {
+//         ans.push_back(i);
+// 		i++;
+//     } else {
+//         i++;
+//     }
+// }
+
+// return ans.size() + 1;
+// }
