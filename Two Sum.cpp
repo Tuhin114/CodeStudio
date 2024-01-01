19232955e1adad217c37a94efd4d0b4a50de3ebe
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-01-01 18:10:57 +0530

    CodeStudio -  Two Sum

---
+++ Two Sum.cpp
@@ -0,0 +1,20 @@
+// CodeStudio -  Two Sum
+//  https://codingninjas.com/studio/problems/reading_6845742?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+// string read(int n, vector<int> book, int target)
+// {
+// int i = 0;
+//         while (i < n - 1) {
+//             int j = i + 1;
+//             while (j < n) {
+//                 if (book[i] + book[j] == target) {
+
+//                     return "YES";
+//                 }
+//                 j++;
+//             }
+//             i++;
+//         }
+//         // If no solution is found, return an empty vector
+//         return "NO";
+//     }
