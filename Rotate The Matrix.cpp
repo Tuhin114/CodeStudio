69caa0a9ad37406d353010d7c71499b0eaba27a4
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-01-06 21:21:07 +0530

    CodeStudio -  Rotate The Matrix

---
+++ Rotate The Matrix.cpp
@@ -0,0 +1,19 @@
+// CodeStudio -  Rotate The Matrix
+// https://www.codingninjas.com/studio/problems/rotate-the-matrix_6825090?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+// #include<vector>
+
+// void rotateMatrix(vector<vector<int>> &mat){
+// 	int n = mat.size();
+//     //transposing the matrix
+//     for (int i = 0; i < n; i++) {
+//     for (int j = 0; j < i; j++) {
+//         swap(mat[i][j], mat[j][i]);
+//     }
+//     }
+//     //reversing each row of the matrix
+//     for (int i = 0; i < n; i++) {
+//     reverse(mat[i].begin(), mat[i].end());
+//     }
+
+// }
