edb105b1680586da5d57cbf7e70ec5f8de4a2258
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-22 23:16:21 +0530

    CodeStudio-Median of two sorted arrays

--- Median of two sorted arrays.cpp
+++ Median of two sorted arrays.cpp
@@ -1,68 +1,54 @@
 // CodeStudio-Median of two sorted arrays
 // https://www.codingninjas.com/studio/problems/median-of-two-sorted-arrays_985294?interviewProblemRedirection=true&leftPanelTabValue=PROBLEM
 
-// // optimal solution - binary search
+// Better Approach
+/**
+ double median(vector<int>& a, vector<int>& b) {
+    int n1 = a.size(), n2 = b.size();
+    int n = n1 + n2;
 
-// // TC = O(log(min(n1, n2))) binary search on the array which has a minimum size
+    //Required indices
+    int ind2 = n/2;
+    int ind1 = ind2 - 1;
+    int cnt = 0;
+    int ind1Elem = -1;
+    int ind2Elem = -1;
 
-// // SC = O(1)
+    //Apply merge sort
+    int i = 0, j = 0;
+    while(i < n1 && j < n2){
+        if(a[i] < b[j]){
+            if (cnt == ind1) ind1Elem = a[i];
+            if (cnt == ind2) ind2Elem = a[i];
+            cnt++;
+            i++;
+        }else{
+            if (cnt == ind1) ind1Elem = b[j];
+            if (cnt == ind2) ind2Elem = b[j];
+            cnt++;
+            j++;
+        }
+    }
 
-// double median(vector<int>& a, vector<int>& b) {
+    //copy the left-out elements:
+    while (i < n1) {
+        if (cnt == ind1) ind1Elem = a[i];
+        if (cnt == ind2) ind2Elem = a[i];
+        cnt++;
+        i++;
+    }
+    while (j < n2) {
+        if (cnt == ind1) ind1Elem = b[j];
+        if (cnt == ind2) ind2Elem = b[j];
+        cnt++;
+        j++;
+    }
 
-//     // take smaller array for binary search
+    //Find the median:
+    if (n % 2 == 1) {
+        return (double)ind2Elem;
+    }
 
-//     if(a.size() > b.size())return median(b, a);
-
-//     int n1 = a.size();
-
-//     int n2 = b.size();
-
-//     int low=0, high=n1;
-
-//     while(low<=high){
-
-//         int cut1 = (low+high) >> 1;
-
-//         int cut2 = (n1+n2+1)/2 - cut1;
-
-//         int left1 = cut1==0 ? INT_MIN : a[cut1-1];
-
-//         int left2 = cut2==0 ? INT_MIN : b[cut2-1];
-
-//         int right1 = cut1==n1 ? INT_MAX : a[cut1];
-
-//         int right2 = cut2==n2 ? INT_MAX : b[cut2];
-
-//         if(left1<=right2 && left2<=right1){
-
-//             if((n1+n2)%2 == 0){
-
-//                 return (max(left1, left2) + min(right1, right2))/2.0;
-
-//             }
-
-//             else{
-
-//                 return max(left1, left2);
-
-//             }
-
-//         }
-
-//         else if(left1 > right2){
-
-//             high = cut1-1;
-
-//         }
-
-//         else{
-
-//             low = cut1+1;
-
-//         }
-
-//     }
-
-//     return 0.0;
-
-// }
+    return (double)((double)(ind1Elem + ind2Elem)) / 2.0;
+}
+*/
