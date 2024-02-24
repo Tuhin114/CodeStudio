df3fbd644f9e71705fd1f9c3625a13d3195ad4c1
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-24 10:24:22 +0530

    Added another solution

--- Largest Element in the Array.cpp
+++ Largest Element in the Array.cpp
@@ -7,3 +7,14 @@
 //     sort(arr.begin(),arr.end());
 //     return arr[n-1];
 // }
+
+// Aliter
+/*
+#include <bits/stdc++.h>
+int largestElement(vector<int> &arr, int n) {
+    int maxi = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        maxi = max(maxi,arr[i]);
+    };
+    return maxi;
+}*/
