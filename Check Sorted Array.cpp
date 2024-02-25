e63fc590433a4e66c294924bbb7099a8f0aabde6
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-25 23:12:35 +0530

    CodeStudio - Check Sorted Array

---
+++ Check Sorted Array.cpp
@@ -0,0 +1,17 @@
+// CodeStudio - Check Sorted Array
+// https://www.codingninjas.com/studio/problems/ninja-and-the-sorted-check_6581957?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+/**
+ int isSorted(int n, vector<int> a) {
+    int i = 0;
+    while(i < n - 1){
+        if(a[i] <= a[i + 1]){
+            i++;
+        }else{
+            return 0;
+        }
+    }
+    return 1;
+}
+
+*/
