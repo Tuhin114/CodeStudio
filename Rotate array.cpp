8efdf626e852db072c56bb6071db08562e72905a
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-27 10:45:06 +0530

    Rotate array Approach 2

--- Rotate array.cpp
+++ Rotate array.cpp
@@ -18,3 +18,19 @@
 // vector<int> rotateArray(vector<int>arr, int k) {
 //     return leftrotate(arr, k%arr.size());
 // }
+
+// Approach 2
+/**
+ class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+
+        vector<int> temp(nums.size());
+        for(int i = 0; i<nums.size(); i++){
+            temp[(i+k)%nums.size()] = nums[i];
+        }
+        //Copy temp into num vector
+        nums = temp;
+    }
+};
+*/
