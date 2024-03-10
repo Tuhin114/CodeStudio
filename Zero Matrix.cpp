d08c7c4410488b7211fc0af727529f5d5c8e1fc8
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-03-10 11:52:36 +0530

    CodeStudio - Zero Matrix

---
+++ Zero Matrix.cpp
@@ -0,0 +1,28 @@
+// CodeStudio - Zero Matrix
+// https://www.codingninjas.com/studio/problems/zero-matrix_1171153?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+/**
+ #include <bits/stdc++.h>
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+
+    vector<int> row(n, 0);
+    vector<int> col(m, 0);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(matrix[i][j] == 0){
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(row[i] || col[j]){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    return matrix;
+}
+*/
