ef0b018d09265986d63f4a675092bbc2b8ec5331
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-05-03 22:29:33 +0530

    CodeStudio -  Bit Manipulation

---
+++ Bit Manipulation.cpp
@@ -0,0 +1,13 @@
+// CodeStudio -  Bit Manipulation
+//  https://www.naukri.com/code360/problems/bit-manipulation_8142533?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+/**
+ vector<int> bitManipulation(int num, int i){
+    int pos = i - 1;
+    bool op1 = num & (1 << pos);
+    int op2 = num | (1 << pos);
+    int op3 = num & ~(1 << pos);
+
+    return {op1, op2, op3};
+}
+*/
